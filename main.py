a93b56aad8098f6ada3c099932960d1208d2b75c
RizkiAK <73286464+RizkiAK@users.noreply.github.com>
2021-08-19 15:40:00 +0700

    tambah resource

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from werkzeug.wrappers import response
+
+# inisiasi objek flask, api, cors
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+identitas = {}
+
+class ContohResource(Resource):
+    def get(self):
+        # response = {'msg':'halo dunia, ini app restfull'}
+
+        return identitas
+
+    def post(self):
+        nama = request.form['nama']
+        umur = request.form['umur']
+        identitas['nama'] = nama
+        identitas['umur'] = umur
+        response = {'msg':'data berhasil dimasukkan'}
+        return response
+
+api.add_resource(ContohResource, '/api', methods=["GET", "POST"])
+
+if __name__=='__main__':
+    app.run(debug=True, port=5005)
