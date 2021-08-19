eab0784726a4fea03f50da06037ec6cd97dcfb55
RizkiAK <73286464+RizkiAK@users.noreply.github.com>
2021-08-19 16:01:07 +0700

    memperbaiki pesan respon

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class ContohResource(Resource):
         umur = request.form['umur']
         identitas['nama'] = nama
         identitas['umur'] = umur
-        response = {'msg':'data berhasil dimasukkan'}
+        response = {'msg':'Alhamdulillah! data berhasil dimasukkan'}
         return response
 
 api.add_resource(ContohResource, '/api', methods=["GET", "POST"])
